47e065c7733944a8d7827c34b7f3008304796925
freemansean <97703152+freemansean@users.noreply.github.com>
2022-01-13 15:51:58 -0500

    Add files via upload

---
+++ OrgChannel5Ghz.py
@@ -0,0 +1,55 @@
+import requests
+import json
+
+print('This script will help you pull data from multiple sites at once')
+print('This version of the script will give you a list of the different 5GHz channels in use across the entire org')
+print('Be sure you open the source code and enter your API key into the Authorization section of the headers variable for this to function')
+# define the org here
+print('Please enter the Org-ID you wish to poll:')
+# org = "9777c1a0-6ef6-11e6-8bbf-02e208b2d34f"
+org = input()
+url1 = 'https://api.mist.com/api/v1/orgs/{}/sites'.format(org)
+
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Token jfrCReQ9MqSxyHJPSzWdedbuZbtizj6UhNRPWqb7OiDWmazQucMl9I7AVjyjKzNh4yK88VEKUqvSdds8FIxFXBHwFobMWeDh'
+}
+
+siteidlist = []
+
+sitelist = requests.get(url1, headers=headers)
+sitejson = json.loads(sitelist.text)
+
+for i in range(len(sitejson)):
+    siteid = sitejson[i]["id"]
+    siteidlist.append(siteid)
+
+print('Here are the different site codes that are being polled within your org')
+print(siteidlist)
+
+channels5 = []
+
+
+for i in range(len(siteidlist)):
+    url2 = "https://api.mist.com/api/v1/sites/{}/stats/devices".format(siteidlist[i])
+    devicestats = requests.get(url2, headers=headers)
+    devicestatsjson = json.loads(devicestats.text)
+    if ('radio_stat' in devicestats.text):
+        print("Score! Active AP hardware found at:")
+        print(siteidlist[i])
+        a = len(devicestatsjson)
+        count = 0
+        while count < a:
+            if ('radio_stat' in devicestatsjson[count]):
+                channel = devicestatsjson[count]["radio_stat"]["band_5"]["channel"]
+                print(channel)
+                channels5.append(channel)
+                count = count+1
+            else:
+                count = count+1
+    else:
+        print("The site below does not have currently active hardware:")
+        print(siteidlist[i])
+
+print('Here are the 5GHz channels in use across the org')
+print(channels5)
